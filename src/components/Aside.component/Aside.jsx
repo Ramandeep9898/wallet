b21dd95393375272892d8eae078aa3134406987a
Ramandeep <ramank6238299@gmail.com>
2022-10-17 16:13:27 +0530

    basic flow

---
+++ src/components/Aside.component/Aside.jsx
@@ -0,0 +1,17 @@
+import "./aside.css";
+
+export const Aside = () => {
+  return (
+    <>
+      <section className="aside-container">
+        <li className="aside-items">
+          <ui className="aside-item">
+            <div className="aside-item-logo"></div>
+            <div className="aside-item-typo">Portfolio</div>
+          </ui>
+          <div className="divider"></div>
+        </li>
+      </section>
+    </>
+  );
+};
